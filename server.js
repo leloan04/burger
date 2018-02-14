319cba82902d964830dc00e8175b9652184e915c
Loan Le <32397554+leloan04@users.noreply.github.com>
2018-02-13 23:06:05 -0500

    updates

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parse4r');
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var expressHandlebars = require("express-handlebars");
+var routes = require("./controllers/burgers_controllers.js");
+
+var app = express();
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(methodOverride("_method"));
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}))
+app.set("view engine", "handlebars");
+app.use("/", routes);
+
+var port = process.envPORT || 3000;
+app.listen(port);
